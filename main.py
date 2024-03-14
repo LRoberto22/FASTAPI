aae01708115e458581ca904b3826afc8c20a14cc
LRoberto22 <157325958+LRoberto22@users.noreply.github.com>
2024-03-14 11:50:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 # Manejar los origenes que se permiten en el microservicio, ponienod la ip del servidor donde se aloja la página
 origins = [
     "http://127.0.0.1:5500", 
-    "http://qcipark.x10.mx",
+     "http://qcipark.x10.mx",
 ]
 
 # Permite acceso completo a los origenes especificados con anterioridad
@@ -105,12 +105,11 @@ def verificar_usuario(codigoUsuario: int = Form(...)):
             return{"existe": True}
         else:
             return{"existe": False}
-
+        cursor.close()
+        cursor.close()
     except Exception as e:
         return {"error": str(e)}
-    finally:
-        if 'cursor' in locals():
-            cursor.close;
+    
 
 # ---------------------------End point para registro---------------------------
 #Insercion en la base de datos en el registro de usuario
@@ -170,13 +169,13 @@ def getEntradaSalida(diaSemanaActual: int = Form(...), usuario: int = Form(...))
 def consultaHorario(usr: int = Form(...)):
     try:
         cursor = conexion.cursor()
-        query = "select entrada, salida, fkdiasemana from horario_usuario where fkusuario = %s;"
-        cursor.execute(query, (usr,))
+        query = f"select entrada, salida, fkdiasemana from horario_usuario where fkusuario = {usr};"
+        cursor.execute(query)
         horario = cursor.fetchall()
         conexion.commit()
         cursor.close() 
         if(horario == 0):
-            return [horario]
+            return [0]
         else:      
             return [horario]
     except Exception as e:
@@ -197,10 +196,10 @@ def guardarHorario(entrada: str = Form(...), salida: str = Form(...), codigoUsua
 
 #---------------------------------------------------------RED NEURONAL---------------------------------------------#
 
-# Esta función se encarga de obtener los datos de la base de datos para un día específico. 
-#Toma un parámetro dia que representa el día de la semana para el que se desean los datos. 
-#Luego, ejecuta consultas SQL para obtener las horas de ingreso y de egreso correspondientes a ese día, convierte las horas 
-#de formato de cadena a objetos de tiempo, y devuelve dos DataFrames, uno para los ingresos y otro para los egresos.
+# # Esta función se encarga de obtener los datos de la base de datos para un día específico. 
+# #Toma un parámetro dia que representa el día de la semana para el que se desean los datos. 
+# #Luego, ejecuta consultas SQL para obtener las horas de ingreso y de egreso correspondientes a ese día, convierte las horas 
+# #de formato de cadena a objetos de tiempo, y devuelve dos DataFrames, uno para los ingresos y otro para los egresos.
 def obtener_datos_de_bd(dia: int):
     cursor = conexion.cursor()
 
@@ -216,10 +215,10 @@ def obtener_datos_de_bd(dia: int):
     return ingresos_data, egresos_data
 
 
-# Esta función se encarga de entrenar un modelo de red neuronal para predecir las horas pico de ingresos o egresos. 
-#Toma dos parámetros: X_train, que representa las características de entrada del modelo (en este caso, las horas del día como enteros), y y_train, que representa 
-#los valores objetivo del modelo (en este caso, la frecuencia de ingresos o egresos para cada hora del día). Utiliza un modelo de red neuronal con una capa oculta 
-#de 100 neuronas y una capa de salida lineal. El modelo se compila con la función de pérdida de error cuadrático medio y el optimizador Adam, y luego se entrena durante 100 épocas.
+# # Esta función se encarga de entrenar un modelo de red neuronal para predecir las horas pico de ingresos o egresos. 
+# #Toma dos parámetros: X_train, que representa las características de entrada del modelo (en este caso, las horas del día como enteros), y y_train, que representa 
+# #los valores objetivo del modelo (en este caso, la frecuencia de ingresos o egresos para cada hora del día). Utiliza un modelo de red neuronal con una capa oculta 
+# #de 100 neuronas y una capa de salida lineal. El modelo se compila con la función de pérdida de error cuadrático medio y el optimizador Adam, y luego se entrena durante 100 épocas.
 def entrenar_red_neuronal(X_train, y_train):
     model = Sequential()
     model.add(Dense(100, input_dim=1, activation='relu'))
@@ -228,11 +227,11 @@ def entrenar_red_neuronal(X_train, y_train):
     model.fit(X_train, y_train, epochs=100, batch_size=10, verbose=0)
     return model
 
-#Esta función se encarga de calcular las horas pico de ingresos y egresos para un día específico utilizando un modelo de red neuronal. 
-#Primero, obtiene los datos de la base de datos llamando a obtener_datos_de_bd(dia). 
-#Luego, preprocesa los datos y entrena modelos de red neuronal para predecir las horas pico de ingresos y egresos utilizando la 
-#función entrenar_red_neuronal(X_train, y_train). Finalmente, hace predicciones utilizando los modelos entrenados y devuelve un diccionario con el día, 
-#la hora pico de ingresos y la hora pico de egresos.
+# #Esta función se encarga de calcular las horas pico de ingresos y egresos para un día específico utilizando un modelo de red neuronal. 
+# #Primero, obtiene los datos de la base de datos llamando a obtener_datos_de_bd(dia). 
+# #Luego, preprocesa los datos y entrena modelos de red neuronal para predecir las horas pico de ingresos y egresos utilizando la 
+# #función entrenar_red_neuronal(X_train, y_train). Finalmente, hace predicciones utilizando los modelos entrenados y devuelve un diccionario con el día, 
+# #la hora pico de ingresos y la hora pico de egresos.
 def calcular_horas_pico_y_actividad_con_red_neuronal(dia: int) -> dict:
     ingresos_data, egresos_data = obtener_datos_de_bd(dia)
 
@@ -255,10 +254,10 @@ def calcular_horas_pico_y_actividad_con_red_neuronal(dia: int) -> dict:
         "hora_pico_egresos": hora_pico_egresos
     }
 
-# Función para calcular la hora menos activa antes de una hora especificada
-#utiliza una red neuronal para predecir la hora menos activa antes de la hora especificada. 
-#La hora devuelta es un valor numérico que representa la hora menos activa antes de la hora especificada. 
-#Luego, en el endpoint /hora_menos_actividad_antes/, convertimos este valor numérico de hora a formato de hora para devolverlo en la respuesta JSON.
+# # Función para calcular la hora menos activa antes de una hora especificada
+# #utiliza una red neuronal para predecir la hora menos activa antes de la hora especificada. 
+# #La hora devuelta es un valor numérico que representa la hora menos activa antes de la hora especificada. 
+# #Luego, en el endpoint /hora_menos_actividad_antes/, convertimos este valor numérico de hora a formato de hora para devolverlo en la respuesta JSON.
 def calcular_hora_menos_actividad_antes_con_red_neuronal(dia: int, hora: time) -> str:
     ingresos_data = obtener_datos_de_bd(dia)
 
@@ -293,87 +292,87 @@ def calcular_hora_menos_actividad_antes_con_red_neuronal(dia: int, hora: time) -
 cursor = conexion.cursor()
 
 
-# Definir una función para calcular la hora pico y la hora de menor actividad para un día específico
+ # Definir una función para calcular la hora pico y la hora de menor actividad para un día específico
 def calcular_horas_pico_y_actividad(dia: int) -> dict:
-    cursor.execute(f"SELECT hora_ingreso FROM ingresos WHERE fkdiasemana = {dia}")
-    ingresos_data = pd.DataFrame(cursor.fetchall(), columns=['hora_ingreso'])
+     cursor.execute(f"SELECT hora_ingreso FROM ingresos WHERE fkdiasemana = {dia}")
+     ingresos_data = pd.DataFrame(cursor.fetchall(), columns=['hora_ingreso'])
 
-    cursor.execute(f"SELECT hora_egreso FROM egresos WHERE fkdiasemana = {dia}")
-    egresos_data = pd.DataFrame(cursor.fetchall(), columns=['hora_egreso'])
+     cursor.execute(f"SELECT hora_egreso FROM egresos WHERE fkdiasemana = {dia}")
+     egresos_data = pd.DataFrame(cursor.fetchall(), columns=['hora_egreso'])
 
-    # Convertir las horas a objetos time
-    ingresos_data['hora_ingreso'] = pd.to_datetime(ingresos_data['hora_ingreso'], format='%H:%M:%S').dt.time
-    egresos_data['hora_egreso'] = pd.to_datetime(egresos_data['hora_egreso'], format='%H:%M:%S').dt.time
+     # Convertir las horas a objetos time
+     ingresos_data['hora_ingreso'] = pd.to_datetime(ingresos_data['hora_ingreso'], format='%H:%M:%S').dt.time
+     egresos_data['hora_egreso'] = pd.to_datetime(egresos_data['hora_egreso'], format='%H:%M:%S').dt.time
 
-    # Calcular la hora pico y la hora de menor actividad para ingresos
-    hora_pico_ingresos = ingresos_data['hora_ingreso'].mode().iloc[0] if not ingresos_data.empty else None
-    hora_menos_actividad_ingresos = ingresos_data['hora_ingreso'][
-        ingresos_data['hora_ingreso'] <= time(17, 0, 0)
-    ].value_counts().idxmin() if not ingresos_data.empty else None
+     # Calcular la hora pico y la hora de menor actividad para ingresos
+     hora_pico_ingresos = ingresos_data['hora_ingreso'].mode().iloc[0] if not ingresos_data.empty else None
+     hora_menos_actividad_ingresos = ingresos_data['hora_ingreso'][
+         ingresos_data['hora_ingreso'] <= time(17, 0, 0)
+     ].value_counts().idxmin() if not ingresos_data.empty else None
 
-    # Calcular la hora pico y la hora de menor actividad para egresos
-    hora_pico_egresos = egresos_data['hora_egreso'].mode().iloc[0] if not egresos_data.empty else None
-    hora_menos_actividad_egresos = egresos_data['hora_egreso'].value_counts().idxmin() if not egresos_data.empty else None
+     # Calcular la hora pico y la hora de menor actividad para egresos
+     hora_pico_egresos = egresos_data['hora_egreso'].mode().iloc[0] if not egresos_data.empty else None
+     hora_menos_actividad_egresos = egresos_data['hora_egreso'].value_counts().idxmin() if not egresos_data.empty else None
 
-    return {
-        "dia": dia,
-        "hora_pico_ingresos": hora_pico_ingresos.strftime('%H:%M:%S') if hora_pico_ingresos else None,
-        "hora_menos_actividad_ingresos": hora_menos_actividad_ingresos.strftime('%H:%M:%S') if hora_menos_actividad_ingresos else None,
-        "hora_pico_egresos": hora_pico_egresos.strftime('%H:%M:%S') if hora_pico_egresos else None,
-        "hora_menos_actividad_egresos": hora_menos_actividad_egresos.strftime('%H:%M:%S') if hora_menos_actividad_egresos else None
-    }
+     return {
+         "dia": dia,
+         "hora_pico_ingresos": hora_pico_ingresos.strftime('%H:%M:%S') if hora_pico_ingresos else None,
+         "hora_menos_actividad_ingresos": hora_menos_actividad_ingresos.strftime('%H:%M:%S') if hora_menos_actividad_ingresos else None,
+         "hora_pico_egresos": hora_pico_egresos.strftime('%H:%M:%S') if hora_pico_egresos else None,
+         "hora_menos_actividad_egresos": hora_menos_actividad_egresos.strftime('%H:%M:%S') if hora_menos_actividad_egresos else None
+     }
 
 @app.get("/horas_actividad/")
 async def obtener_horas_actividad(dia: int):
-    horas_actividad = calcular_horas_pico_y_actividad(dia)
-    return horas_actividad
+     horas_actividad = calcular_horas_pico_y_actividad(dia)
+     return horas_actividad
 
 def calcular_hora_menos_actividad_antes(dia: int, hora: time):
-    hora_ajustada = datetime.combine(datetime.today(), hora) - timedelta(minutes=15)
-    hora_ajustada_str = hora_ajustada.strftime('%H:%M:%S')
+     hora_ajustada = datetime.combine(datetime.today(), hora) - timedelta(minutes=15)
+     hora_ajustada_str = hora_ajustada.strftime('%H:%M:%S')
     
-    cursor.execute(f"SELECT hora_ingreso FROM ingresos WHERE fkdiasemana = {dia} AND hora_ingreso < '{hora}' ORDER BY hora_ingreso DESC")
-    ingresos_data = pd.DataFrame(cursor.fetchall(), columns=['hora_ingreso'])
+     cursor.execute(f"SELECT hora_ingreso FROM ingresos WHERE fkdiasemana = {dia} AND hora_ingreso < '{hora}' ORDER BY hora_ingreso DESC")
+     ingresos_data = pd.DataFrame(cursor.fetchall(), columns=['hora_ingreso'])
     
-    if not ingresos_data.empty:
-        hora_menos_actividad_antes = ingresos_data['hora_ingreso'].value_counts().idxmin()
-    else:
-        hora_menos_actividad_antes = hora_ajustada_str
+     if not ingresos_data.empty:
+         hora_menos_actividad_antes = ingresos_data['hora_ingreso'].value_counts().idxmin()
+     else:
+         hora_menos_actividad_antes = hora_ajustada_str
     
-    return hora_menos_actividad_antes
+     return hora_menos_actividad_antes
 
 @app.get("/hora_menos_actividad_antes/")
 async def obtener_hora_menos_actividad_antes(dia: int, hora: time):
-    hora_menos_actividad_antes = calcular_hora_menos_actividad_antes(dia, hora)
+     hora_menos_actividad_antes = calcular_hora_menos_actividad_antes(dia, hora)
     
-    return {
-        "dia": dia,
-        "hora_especificada": hora.strftime('%H:%M:%S'),
-        "hora_menos_actividad_antes": str(hora_menos_actividad_antes)
-    }
+     return {
+         "dia": dia,
+         "hora_especificada": hora.strftime('%H:%M:%S'),
+         "hora_menos_actividad_antes": str(hora_menos_actividad_antes)
+     }
 
 
 @app.get("/actividad_promedio_semanal/")
 async def obtener_actividad_promedio_semanal():
-    actividad_promedio = {}
+     actividad_promedio = {}
     
-    # Obtener el recuento de ingresos por día de la semana
-    cursor.execute("SELECT fkdiasemana, COUNT(*) AS cantidad FROM ingresos GROUP BY fkdiasemana")
-    resultados = cursor.fetchall()
+     # Obtener el recuento de ingresos por día de la semana
+     cursor.execute("SELECT fkdiasemana, COUNT(*) AS cantidad FROM ingresos GROUP BY fkdiasemana")
+     resultados = cursor.fetchall()
     
-    # Calcular el total de ingresos de la semana
-    total_semana = sum(cantidad for _, cantidad in resultados)
+     # Calcular el total de ingresos de la semana
+     total_semana = sum(cantidad for _, cantidad in resultados)
     
-    # Calcular el porcentaje de actividad para cada día de la semana
-    for dia, cantidad in resultados:
-        porcentaje_actividad = (cantidad / total_semana) * 100
-        actividad_promedio[dia] = porcentaje_actividad
+     # Calcular el porcentaje de actividad para cada día de la semana
+     for dia, cantidad in resultados:
+         porcentaje_actividad = (cantidad / total_semana) * 100
+         actividad_promedio[dia] = porcentaje_actividad
     
-    return actividad_promedio
+     return actividad_promedio
 
 
 
 # Constructor y carga de microservicio
 #if __name__ == "__main__":
  #   import uvicorn
-#uvicorn.run(app, host="0.0.0.0", port=8000)
+  #  uvicorn.run(app, host="0.0.0.0", port=8000)
